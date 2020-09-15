40efd2feecf2d656376d4c88e958f71c872f9654
Jacob <jayjayb100@msn.com>
2020-09-15 18:20:36 -0500

    first commit

---
+++ precache-manifest.eb4ce6d1c91df05678076c221c68ef48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deeed39ea4763dfc422e1d08247e53e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a862e95a7dab4f30c2dc",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "a9d9838edb8693f9fcf8",
+    "url": "/static/css/main.005bf12a.chunk.css"
+  },
+  {
+    "revision": "a862e95a7dab4f30c2dc",
+    "url": "/static/js/2.b77ead87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b77ead87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d9838edb8693f9fcf8",
+    "url": "/static/js/main.d04492f0.chunk.js"
+  },
+  {
+    "revision": "9fe3687da5704872eaf7",
+    "url": "/static/js/runtime-main.352792a7.js"
+  }
+]);
